7f01153d07757dedf932421ac3f59c5707ed6f80
SHUB2205 <111409237+SHUB2205@users.noreply.github.com>
2023-02-18 12:47:58 -0500

    Add files via upload
    Download speech_recognition package before running  the program.

---
+++ Speech to text.py
@@ -0,0 +1,13 @@
+for i in range(1,10000000000000000):
+    import speech_recognition as sr  
+    r = sr.Recognizer()  
+    with sr.Microphone() as source:  
+        print("Speak Anything :")  
+        audio = r.listen(source)  
+        try:  
+            text = r.recognize_google(audio)  
+            print("You said : {}".format(text))  
+
+
+        except:  
+            print("Sorry could not recognize what you said") 
